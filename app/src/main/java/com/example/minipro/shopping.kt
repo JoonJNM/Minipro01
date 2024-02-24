67b438f7baf67ccd66a974aab9bc9da9317276ff
JoonJNM <jjunemee@gmail.com>
2024-02-24 17:47:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/minipro/shopping.kt
@@ -0,0 +1,11 @@
+package com.example.minipro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class shopping : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
